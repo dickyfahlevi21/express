3c058c6bb30f3abf773c5250c5f699a50150bf35
dickyfahlevi21 <dicky.fahlevi21@gmail.com>
2020-08-03 23:43:27 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,108 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const port = 3001;
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+// mongoose.connect('mongodb://localhost/todolist', {
+//   useNewUrlParser: true
+// });
+mongoose.connect("mongodb://localhost/todolist", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
+
+app.set("view engine", "pug");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+const todoShema = new mongoose.Schema({
+  name: String,
+  status: Boolean,
+});
+
+const TodoModel = mongoose.model("Todo", todoShema);
+
+const fethcuser = async () => {
+  return axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((response) => {
+      return response.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const getTodo = async () => {
+  return await TodoModel.find({});
+};
+
+app.get("/", (req, res) => {
+  res.send("Hai husen");
+});
+
+app.get("/user/:userid?", async (request, response) => {
+  const {
+    params
+  } = request;
+  const userList = await fethcuser();
+  let user = null;
+  if (params.userid !== undefined) {
+    user = await userList.find((e) => e.id === parseInt(params.userid));
+    response.render("userdetail", {
+      user
+    });
+  } else {
+    response.render("user", {
+      users: userList
+    });
+  }
+  // response.json({ id: params.userid, user, userList });
+});
+
+app.get("/pug", function (req, res) {
+  res.render("index", {
+    title: "Hey",
+    message: "Hello there!"
+  });
+});
+
+app.get("/todo", async (req, res) => {
+  const todolist = await getTodo()
+    .then((response) => {
+      console.log(response);
+      res.render("todo", {
+        todo: response
+      });
+    })
+    .catch((err) => console.log(err));
+});
+
+app.post("/todo", async (req, res) => {
+  const addTodo = new TodoModel({
+    name: req.body.todo
+  });
+  await addTodo.save((err, todo) => {
+    console.log("saved");
+  });
+  console.log(addTodo);
+  res.redirect("/todo");
+});
+
+app.post("/todo/:id", (req, res) => {
+  // const todo = TodoModel.findById(req.params.id);
+  res.json({
+    id: req.params.id,
+    body: req.body
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
